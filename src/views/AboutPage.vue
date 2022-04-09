19b699a68264e3502dd2d7fa4cf12398fd4d8a21
bvander <benoit.vanderstappen@sony.com>
2022-04-10 00:21:06 +0200

    Fix AboutPage

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -6,7 +6,7 @@
         <img src="@/assets/img/Logo-bb.png" alt="Logo Baden Battle" />
       </div>
       <div class="about-info">
-        <ion-list lines="none">
+        <ion-list lines="none" class="ion-padding-vertical" style="border-radius: 5px">
           <ion-item>
             <ion-icon :icon="calendar" slot="start"></ion-icon>
             <ion-label>23 Avril 2022</ion-label>
@@ -21,15 +21,13 @@
             </ion-label>
           </ion-item>
 
-          <ion-grid>
-            <ion-row>
+
               <ion-item>
                 <ion-icon :icon="informationCircleOutline" slot="start"></ion-icon>
                 <ion-label>A propos</ion-label>
               </ion-item>
-            </ion-row>
-            <ion-row>
-              <p class="ion-padding-start ion-padding-end">
+
+              <p class="ion-padding-start ion-padding-end p-left">
                 Il sʼagit dʼun grand rassemblement scout, guide et patro à destination des étincelles, lutins, louvettes, louveteaux et chevaliers
                 (donc de 8 à 12 ans, filles et garçons) mais également depuis cette année à destination des plus jeunes âgés de 5 à 7 ans (baladins et
                 nutons), organisé par le poste pionnier Peter Pan de Soignies. Durant toute la journée, les participants passent à travers de
@@ -41,17 +39,16 @@
                 <a href="https://www.facebook.com/badenbattle/">page Facebook</a> ou via notre
                 <a href="mailto:infobadenbattle@gmail.com">adresse email</a>.
               </p>
-            </ion-row>
-          </ion-grid>
+
         </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { calendar, pin, informationCircleOutline } from 'ionicons/icons';
-import { IonContent, IonPage, IonIcon, IonLabel, IonRow, IonList, IonItem, IonGrid } from "@ionic/vue";
+import { IonContent, IonPage, IonIcon, IonLabel, IonList, IonItem, IonText} from "@ionic/vue";
 import HeaderTemplate from "@/components/HeaderTemplate.vue";
 
 </script>
@@ -84,8 +81,5 @@ import HeaderTemplate from "@/components/HeaderTemplate.vue";
   margin-inline-end: 20px;
 }
 
-.ios .about-info {
-  text-align: center;
-}
 
 </style>
