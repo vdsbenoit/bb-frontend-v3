cfea440eb5d955fde8c7c24db1e84174581036de
bvander <benoit.vanderstappen@sony.com>
2022-04-10 17:55:04 +0200

    Add schedule in the settings

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,4 +1,4 @@
-import { useAuthStore } from './users';
+
 import { magnetar } from "./magnetar";
 import { defineStore } from "pinia";
 
@@ -9,6 +9,11 @@ const SETTINGS_DOCUMENT = "app";
 /// configuration //
 //////////////////
 
+interface Schedule {
+  start: string;
+  stop: string;
+}
+
 // App settings (from firestore, through magneta)
 export interface AppSetting {
   lastGameDbUpdate: Date; // last time the game DB was updated on the remote, to avoid streaming it
@@ -18,6 +23,7 @@ export interface AppSetting {
   gameLeaderSections: string[];
   everyoneCanSetScoreAnywhere: boolean;
   leaderRegistration: boolean; // true when the leader can register to games
+  schedule: Schedule[];
 }
 
 const appSettingsDefaults = {
@@ -28,12 +34,23 @@ const appSettingsDefaults = {
   gameLeaderSections: [],
   everyoneCanSetScoreAnywhere: false,
   leaderRegistration: true, // fixme: change to false
+  schedule: [] as Schedule[],
 };
 
 function appSettingsDefaultsFunc(payload?: Partial<AppSetting>): AppSetting {
-  return { ...appSettingsDefaults, ...payload }
+  const appSettingsDef = {
+    lastGameDbUpdate: new Date(0),
+    maxGameLeaders: 2,
+    freezeScore: true,
+    categories: [],
+    gameLeaderSections: [],
+    everyoneCanSetScoreAnywhere: false,
+    leaderRegistration: true, // fixme: change to false
+    schedule: [],
+  };
+  return { ...appSettingsDef, ...payload }
 }
-const appSettingsModule = magnetar.doc(`${SETTINGS_COLLECTION}/${SETTINGS_DOCUMENT}`, {
+const appSettingsModule = magnetar.doc<AppSetting>(`${SETTINGS_COLLECTION}/${SETTINGS_DOCUMENT}`, {
   modifyPayloadOn: { insert: appSettingsDefaultsFunc },
   modifyReadResponseOn: { added: appSettingsDefaultsFunc },
 });
@@ -96,4 +113,32 @@ export const setlastGameDbUpdate = async () => {
     console.error("appSettingsModule not loaded, setting default value for lastGameDbUpdate");
   }
   localStore.lastGameDbUpdate = appSettingsDefaults.lastGameDbUpdate;
+}
+
+export const setSchedule = async (schedule: Schedule[]) => {
+  return appSettingsModule.merge({ schedule });
+} 
+
+// fixme
+export const hardcodeSchedule = async () => {
+  const schedule = [
+    {start: "10h09", stop: "10h24"},
+    {start: "10h27", stop: "10h42"},
+    {start: "10h45", stop: "11h00"},
+    {start: "11h03", stop: "11h18"},
+    {start: "11h23", stop: "11h36"},
+    {start: "11h39", stop: "11h54"},
+    {start: "11h57", stop: "12h12"},
+    {start: "13h15", stop: "13h30"},
+    {start: "13h33", stop: "13h48"},
+    {start: "13h51", stop: "14h06"},
+    {start: "14h09", stop: "14h24"},
+    {start: "14h27", stop: "14h42"},
+    {start: "14h45", stop: "15h00"},
+    {start: "15h15", stop: "15h30"},
+    {start: "15h33", stop: "15h48"},
+    {start: "15h51", stop: "16h06"},
+    {start: "16h09", stop: "16h24"},
+  ]
+  return appSettingsModule.merge({ schedule });
 }
