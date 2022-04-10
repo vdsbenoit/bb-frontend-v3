e4cb93f9304bb064e6a698f692729a68b14c2829
bvander <benoit.vanderstappen@sony.com>
2022-04-10 23:10:30 +0200

    Add back button

--- src/components/HeaderTemplate.vue
+++ src/components/HeaderTemplate.vue
@@ -1,16 +1,24 @@
 <template>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button color="primary"></ion-menu-button>
-        </ion-buttons>
-        <ion-title>{{ props.pageTitle }}</ion-title>
-      </ion-toolbar>
-    </ion-header>
+  <ion-header :translucent="true">
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-back-button></ion-back-button>
+      </ion-buttons>
+      <ion-buttons slot="start">
+        <ion-menu-button color="primary"></ion-menu-button>
+      </ion-buttons>
+      <ion-title>{{ props.pageTitle }}</ion-title>
+    </ion-toolbar>
+  </ion-header>
 </template>
 
 <script setup lang="ts">
-import { IonButtons, IonHeader, IonMenuButton, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineProps } from "vue";
-const props = defineProps(['pageTitle']);
+import { IonButtons, IonHeader, IonMenuButton, IonTitle, IonToolbar, IonBackButton } from "@ionic/vue";
+import { defineProps,   } from "vue";
+const props = defineProps(["pageTitle"]);
 </script>
+<style scoped>
+.can-go-back ion-menu-button {
+    display: none;
+}
+</style>
