524cc1221a4e8c526bceef4222fe7e8c2b5e5498
vdsbenoit <vdsbenoit@gmail.com>
2024-03-31 18:48:55 -0500

    v0.3.0

--- src/app/buildinfo.ts
+++ src/app/buildinfo.ts
@@ -1,5 +1,5 @@
 export const buildInfo = {
-  buildVersion: "0.2.7",
-  buildDate: 1696245241477,
-  buildDateStr: "Mon Oct 02 2023 13:14:01 GMT+0200 (GMT+02:00)"
+  buildVersion: "0.3.0",
+  buildDate: 1711927287120,
+  buildDateStr: "Sun Mar 31 2024 18:21:27 GMT-0500 (Colombia Standard Time)"
 }
