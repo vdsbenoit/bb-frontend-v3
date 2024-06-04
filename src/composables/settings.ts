0fbf0e07baa5bd95e63cd5ba3cb10d48e0baded2
vdsbenoit <vdsbenoit@gmail.com>
2024-06-04 16:31:17 +0200

    Settings composable

--- src/composables/settings.ts
+++ src/composables/settings.ts
@@ -1,7 +1,7 @@
 // Constants & db references
 
 import { db } from "@/services/firebase";
-import { collection, doc } from "firebase/firestore";
+import { collection, doc, updateDoc } from "firebase/firestore";
 import { useDocument } from "vuefire";
 
 const APP_COLLECTION_NAME = "app"
@@ -45,4 +45,37 @@ export function useAppSettings() {
 }
 export function useAppConfig() {
   return useDocument<AppConfig>(CONFIG_DOC_REF)
+}
+
+// Setters
+
+export const updateAppSettings = async (settingsData: any) => {
+  return updateDoc(SETTINGS_DOC_REF, settingsData)
+} 
+export const setSchedule = async (schedule: Schedule[]) => {
+  return updateDoc(CONFIG_DOC_REF, { schedule })
+} 
+
+// fixme
+export const hardcodeSchedule = async () => {
+  const schedule = [
+    {start: "10h09", stop: "10h24"},
+    {start: "10h27", stop: "10h42"},
+    {start: "10h45", stop: "11h00"},
+    {start: "11h03", stop: "11h18"},
+    {start: "11h23", stop: "11h36"},
+    {start: "11h39", stop: "11h54"},
+    {start: "11h57", stop: "12h12"},
+    {start: "13h15", stop: "13h30"},
+    {start: "13h33", stop: "13h48"},
+    {start: "13h51", stop: "14h06"},
+    {start: "14h09", stop: "14h24"},
+    {start: "14h27", stop: "14h42"},
+    {start: "14h45", stop: "15h00"},
+    {start: "15h15", stop: "15h30"},
+    {start: "15h33", stop: "15h48"},
+    {start: "15h51", stop: "16h06"},
+    {start: "16h09", stop: "16h24"},
+  ]
+  return updateDoc(CONFIG_DOC_REF, { schedule })
 }
