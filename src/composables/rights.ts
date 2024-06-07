9c39bdbac9736ec0779f086be5ad40c02b3f836f
vdsbenoit <vdsbenoit@gmail.com>
2024-06-07 16:43:51 +0200

    Improve rights util

--- src/composables/rights.ts
+++ src/composables/rights.ts
@@ -1,130 +1,123 @@
 import { useCurrentUserProfile } from "@/composables/userProfile";
 import { ROLES } from '@/constants';
 import { RefGame } from "@/types";
-import { ref, watchEffect } from "vue";
+import { computed, ref, watchEffect } from "vue";
 import { useAppSettings } from "./settings";
 
-export function useCanEditScores(game: RefGame) {
-  const canEditGameScore = ref(false)
+export function useCanEditScores(rGame: RefGame) {
   const currentUserProfile = useCurrentUserProfile()
   const appSettings = useAppSettings()
 
-  watchEffect(() => {
-    if (! game.value) {
+  return computed(() => {
+    if (!rGame.value) {
       console.debug("Cannot set score, game variable is not set yet")
-      canEditGameScore.value = false
-      return
+      return false
     }
     if (!currentUserProfile.value) {
       console.debug("Cannot set score, user is not authenticated")
-      canEditGameScore.value = false
-      return
+      return false
     }
 
     // Check frozen score
     if (!appSettings.value?.canSetScores) {
       console.debug("Cannot set score. Score registration is not enabled yet")
-      canEditGameScore.value = false
-      return
+      return false
     }
     // Check if not leader
     if (currentUserProfile.value.role < ROLES.Animateur) {
-      console.debug(`User ${currentUserProfile.value.id} cannot edit game ${game.value.id} score. Insufficient role`)
-      canEditGameScore.value = false
-      return
+      console.debug(`User ${currentUserProfile.value.id} cannot edit game ${rGame.value.id} score. Insufficient role`)
+      return false
     }
     // Check if moderator
     if (currentUserProfile.value.role >= ROLES.Organisateur) {
-      canEditGameScore.value = true
-      return
+      return true
     }
     // Check if global setting allow leaders to set any scores
     if (appSettings.value.canSetAnyScores) {
-      canEditGameScore.value = true
-      return
+      return true
     }
-    // Check if leader assigned to the game
+    // Check if leader is assigned to at least a game
+    if (!currentUserProfile.value.games) {
+      console.debug(`User ${currentUserProfile.value.id} is not registered to set score in any games`)
+      return false
+    }
+    // Check if leader is assigned to the game
+    const gameAttendantsIds = rGame.value.attendants.map(attendant => attendant.user.id)
+    const userGamesIds = currentUserProfile.value.games.map(g => g.game.id)
     if (
-      game.value.morningLeaders.includes(currentUserProfile.value.id) &&
-      currentUserProfile.value.morningGame == game.value.id
+      gameAttendantsIds.includes(currentUserProfile.value.id) &&
+      userGamesIds.includes(rGame.value.id)
     ) {
-      canEditGameScore.value = true
-      return
+      return true
     }
-    if (
-      game.value.afternoonLeaders.includes(currentUserProfile.value.id) &&
-      currentUserProfile.value.afternoonGame == game.value.id
-    ) {
-      canEditGameScore.value = true
-      return
-    }
-    console.debug(`Cannot set score, user ${currentUserProfile.value.id} is not registered at ${game.value.id}`)
-    canEditGameScore.value = false
+    console.debug(`Cannot set score, user ${currentUserProfile.value.id} is not registered at ${rGame.value.id}`)
+    return false 
   })
-
-  return canEditGameScore
 }
 
 // todo: check that these rules match with the setLeaders setters
 export function useCanRegister() {
-  const result = ref(false)
-  const reason = ref("")
   const currentUserProfile = useCurrentUserProfile()
   const appSettings = useAppSettings()
 
-  watchEffect(() => {
+  return computed(() => {
     if (!currentUserProfile.value) {
-      reason.value = "User cannot register to a game. User is not authenticated"
-      result.value = false
-      return
+      return {
+        result : false,
+        reason : "User cannot register to a game. User is not authenticated"
+      }
     }
     if (currentUserProfile.value.role == ROLES.Animateur || currentUserProfile.value.role == ROLES.Chef) {
-      reason.value = `User  ${currentUserProfile.value.id} cannot register to a game. Insufficient role`
-      result.value = false
-      return
+      return {
+        result: false,
+        reason: `User  ${currentUserProfile.value.id} cannot register to a game. Insufficient role`
+      }
     }
     if (!appSettings.value?.leaderRegistration) {
-      reason.value = `Leader registration is currently closed`
-      result.value = false
-      return
+      return {
+        result: false,
+        reason: `Leader registration is currently closed`
+      }
     }
-    reason.value = ""
-    result.value = true
+    return { result: true, reason: "" }
   })
-
-  return { result, reason }
 }
 
 export function useCanRegisterSomeone() {
-  const result = ref(false)
-  const reason = ref("")
   const currentUserProfile = useCurrentUserProfile()
   const appSettings = useAppSettings()
 
-  watchEffect(() => {
+  return computed(() => {
     if (!currentUserProfile.value) {
-      reason.value = "The user cannot register someone to a game. The user is not authenticated"
-      result.value = false
-      return
+      return {
+        result: false,
+        reason: "The user cannot register someone to a game. The user is not authenticated"
+      }
     }
     if (currentUserProfile.value.role >= ROLES.Organisateur) {
-      reason.value = ""
-      result.value = true
-      return
+      return { result: true, reason: "" }
     }
     if (currentUserProfile.value.role < ROLES.Chef) {
-      reason.value = `User  ${currentUserProfile.value.id} cannot register someone to a game. Insufficient role`
-      result.value = false
-      return
+      return {
+        result: false,
+        reason: `User  ${currentUserProfile.value.id} cannot register someone to a game. Insufficient role`
+      }
     }
     if (!appSettings.value?.leaderRegistration) {
-      reason.value = `Leader registration is currently closed`
-      result.value = false
-      return
+      return {
+        result: false,
+        reason: `Leader registration is currently closed`
+      }
     }
-    reason.value = ""
-    result.value = true
+    return { result: true, reason: "" }
   })
 
-  return { result, reason }
+}
+
+export function useCanEditGames(){
+  const currentUserProfile = useCurrentUserProfile()
+  return computed(() => {
+    if(!currentUserProfile.value)  return false
+    return currentUserProfile.value.role >= ROLES.Organisateur
+  })
 }
